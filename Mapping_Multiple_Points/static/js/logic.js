780fd2e94be679393d1a86247c69990537651d75
Rita Ezeoke <ezeokechibuzor333@gmail.com>
2022-12-07 23:25:44 -0500

    new update

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -4,23 +4,10 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
-      36.1733, -120.1794
+      40.7, -94.5
     ],
-    zoom: 7
+    zoom: 4
   });
-
-  // Coordinates for each point to be used in the line.
-let line = [
-  [33.9416, -118.4085],
-  [37.6214, -122.3790],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088]
-];
-
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-  color: "yellow"
-}).addTo(map);
 
 //  Add a marker to the map for Los Angeles, California.
 
@@ -38,7 +25,7 @@ cityData.forEach(function(city) {
 });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
